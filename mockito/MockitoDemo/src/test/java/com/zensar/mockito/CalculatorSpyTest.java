30f57943dfb1a194c15a053a8d457e7ad9e71fed
MansiHiremath <hiremathmansi1548@gmail.com>
2019-10-25 04:03:51 -0700

    Add files via upload

---
+++ mockito/MockitoDemo/src/test/java/com/zensar/mockito/CalculatorSpyTest.java
@@ -0,0 +1,18 @@
+package com.zensar.mockito;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class CalculatorSpyTest {
+	Calculator calculator=new Calculator();
+	@Test
+	public void addTest1() {
+		Calculator calculatorSpy=Mockito.spy(Calculator.class);
+		//Mockito.when(calculatorSpy.add(100, 50)).thenReturn(100);
+		Mockito.when(calculatorSpy.add(100, 50)).thenCallRealMethod();
+		assertEquals(150,calculatorSpy.add(100, 50));
+	}
+
+}
